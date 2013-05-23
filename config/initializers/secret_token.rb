edaeeee84eebda7c243e1b16557b81f93f5170cc
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-23 12:35:01 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScreenOverflow::Application.config.secret_token = 'c1373da5caed232109242eb00cf2ecd771d58ba25a84bf5f1cc695a349c03af86ac84a2bbf44eeef93be62c3720cc14df80c71d35a6ef2456b76bfa23fcd2e18'
