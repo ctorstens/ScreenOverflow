0c913e16b234c1ca6f53e9181a3da63ae33cb86f
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-24 22:17:06 -0700

    complete initial posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
 	def index
+		@posts = Post.all
 	end
 
 	def new
@@ -19,12 +20,17 @@ class PostsController < ApplicationController
 	end
 
 	def show
+		@post = Post.find(params[:id])
 	end
 
 	def edit
+		@post = Post.find(params[:id])
 	end
 
 	def update
+		@post = Post.find(params[:id])
+		@post.update_attributes(params[:post])
+		redirect_to post_path(@post)
 	end
 
 	def destroy
