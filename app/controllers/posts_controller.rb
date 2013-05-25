6ea195963e391731f1d12a4e3d9110d350bb32dd
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-24 22:10:57 -0700

    add skeleton methods to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
 
+	def index
+	end
+
 	def new
 		@post = Post.new
 	end
@@ -14,4 +17,17 @@ class PostsController < ApplicationController
 			render :new
 		end
 	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
 end
