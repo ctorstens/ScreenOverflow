1fba519bac0180624bba939c496ae86ba7bfe98d
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-25 18:47:46 -0700

    comment out static google config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,11 @@
 ScreenOverflow::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  GOOGLE_CONFIG = YAML.load_file("#{::Rails.root}/config/google_auth.yml")
+  # GOOGLE_CONFIG = YAML.load_file("#{::Rails.root}/config/google_auth.yml")
 
-  ENV['GOOGLE_CLIENT_ID'] = GOOGLE_CONFIG['google_client_id']
-  ENV['GOOGLE_CLIENT_SECRET'] = GOOGLE_CONFIG['google_secret']
-  ENV['GOOGLE_REDIRECT_URI'] = GOOGLE_CONFIG['google_redirect_uri']
+  # ENV['GOOGLE_CLIENT_ID'] = GOOGLE_CONFIG['google_client_id']
+  # ENV['GOOGLE_CLIENT_SECRET'] = GOOGLE_CONFIG['google_secret']
+  # ENV['GOOGLE_REDIRECT_URI'] = GOOGLE_CONFIG['google_redirect_uri']
   
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
