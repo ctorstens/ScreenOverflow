5a22bb4ace0f5358ba6a6b272cc0df4c367ff4a5
Kristine Hines <Hines.kristine@gmail.com>
2013-05-26 20:04:57 -0700

    removed debugging statements again

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -10,32 +10,24 @@ $(document).ready(function() {
 
 var postUpVoteSuccess = function() {
   $(".post form#vote_up_post_form").on('ajax:success', function(event, data){
-    console.log("postup");
-    console.log(data.vote_total);
     $(".post_vote_score").text(data.vote_total)
   });
 };
 
 var postDownVoteSuccess = function() {
   $(".post form#vote_down_post_form").on('ajax:success', function(event, data){
-    console.log("postdown");
-    console.log(data.vote_total);
     $(".post_vote_score").text(data.vote_total)
   });
 };
 
 var commentUpVoteSuccess = function() {
   $(".comment form.vote_up_comment_form").on('ajax:success', function(event, data){
-    console.log("commentup");
-    console.log(data.vote_total);
     $(this).parent().find("span.vote_score").text(data.vote_total)
   });
 };
 
 var commentDownVoteSuccess = function() {
   $(".comment form.vote_down_comment_form").on('ajax:success', function(event, data){
-    console.log("commentdown");
-    console.log(data.vote_total);
     $(this).parent().find("span.vote_score").text(data.vote_total)
   });
 };
