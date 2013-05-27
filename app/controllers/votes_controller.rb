57aface0689894b18f7c5652c82bf53c32cf6470
Kristine Hines <Hines.kristine@gmail.com>
2013-05-26 19:20:07 -0700

    can upvote and downvote comments and posts with ajax, dryed up votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,17 +1,22 @@
 class VotesController < ApplicationController
 
-  def create
-    p params[:post][:vote]
+  def post_vote
     @post = Post.find(params[:post][:id])
-
-    if params[:post][:vote] == 'like'
-      @post.liked_by current_user
-      p "LIKE"
-    else
-      @post.downvote_from current_user
-      p "DISLIKE"
-    end
-
+    vote(@post, params[:post][:vote])
     render :json => {:vote_total => (@post.likes.size - @post.dislikes.size)}, :status => :ok
+  end
+
+  def comment_vote
+    @comment = Comment.find(params[:id])
+    vote(@comment, params[:comment][:vote])
+    render :json => {:vote_total => (@comment.likes.size - @comment.dislikes.size), :id => @comment.id}, :status => :ok
+  end
+end
+
+def vote(object, vote)
+  if vote == 'like'
+    object.liked_by current_user
+  else
+    object.downvote_from current_user
   end
 end
