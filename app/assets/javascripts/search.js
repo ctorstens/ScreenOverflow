51ccd54f028fee9996cf02a7e25df843dca73414
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-28 12:03:16 -0700

    change name attribute in 'typeahead' to fix search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,16 +2,16 @@
 $(document).ready(function () {
   $('#typeahead_search').typeahead([
     {
-      name: 'users',
-      // remote: 'users/search',
+      name: 'search-users',
       prefetch: 'users/search',
+      // prefetch: 'users/search',
       // local: ["Chris Torstenson", "Wilton Hodkiewicz IV", "Eula Marks", "Ms. Roxanne Schumm", "Makayla McGlynn DDS", "Ms. Robb Greenfelder", "Gerry Ferry", "Frederick Larkin", "Trevion Rutherford", "Dr. Robb Murphy", "Virgil Bernhard"],
       header: "<h3 class='category_name'>Users</h3>",
       limit: 3
     },
 
     {
-      name: 'posts',
+      name: 'search-posts',
       // remote: 'posts/search',
       prefetch: 'posts/search',
       header: "<h3 class='category_name'>Posts</h3>",
@@ -19,11 +19,12 @@ $(document).ready(function () {
     },
 
     {
-      name: 'tags',
+      name: 'search-tags',
       // remote: 'tags/search',
       prefetch: 'tags/search',
       header: "<h3 class='category_name'>Tags</h3>",
       limit: 3
     }
+
   ]);
 });
