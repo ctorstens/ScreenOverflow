dee423ec8fb0684282943aa843c3c3b8d52e0c39
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-28 01:57:24 -0700

    Revert "Merge branch 'public_activity' into development"
    This reverts commit 0d1ec74053d92029b984b368cf3ecdc1d674cdc8, reversing
    changes made to e887f6b5de21f27bc9f3900169f5dcf25c74151e.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  include PublicActivity::StoreController 
+
   helper_method :current_user
   helper_method :yt_client
+
+  private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,4 @@
 class Tag < ActiveRecord::Base
-  include PublicActivity::Model
-  tracked owner: Proc.new{ |controller, model| controller.current_user }
-  
   has_and_belongs_to_many :posts
   attr_accessible :name
 
