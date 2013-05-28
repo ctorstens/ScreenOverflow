e35d9544d4c065084a6c541814a1571b4538a4e4
Apprentice <apprentice@devbootcamp.com>
2013-05-27 18:22:02 -0700

    added unit testing for voting on posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
+require 'acts_as_votable'
 
 describe Post do
 	let(:user) {build(:user)}
 	let(:post) {build(:post)}
+	let(:post_with_tags) {create(:post_with_tags)}
 
 	describe "can create a new post" do
 
@@ -82,10 +84,58 @@ describe Post do
 				post.youtube?("http://crazy.be/payDsNKcRKQ").should eq(false)
 			end				
 		end
-
-
-
 	end
+
+  describe  "post with tags" do
+    context "with valid tags" do
+      it "should create new tags" do
+        create(:post_with_tags).should be_valid
+      end
+
+      it "tag_list should have 3 length" do 
+        post_with_tags.tag_list.length.should eq(3)
+      end
+    end
+  end
+
+  describe  "post with comment" do
+    context "with valid comments" do
+      it "should create new comment" do
+        create(:post_with_comments).should be_valid
+      end
+    end
+  end
+
+   describe  "post with votes" do
+   	let(:post) {create(:post)}
+   	let(:user1) {create(:user)}
+   	let(:user2) {create(:user)}
+
+    context "can vote on post" do
+      it "should allow users to upvote" do
+        expect {post.liked_by(user1)}.to change{post.likes.size}.by(1)
+      end
+
+      it "should allow users to downvote" do
+        expect {post.disliked_by(user1)}.to change{post.dislikes.size}.by(1)
+      end
+    end
+
+    context "can unvote on post" do
+    	it "should allow users to unvote" do
+    		post.liked_by(user2)
+    		expect {post.unliked_by :voter => user2}.to change{post.likes.size}.by(-1)
+    	end
+
+    	it "should allow users to unvote" do
+    		post.disliked_by(user2)
+    		expect {post.undisliked_by :voter => user2}.to change{post.dislikes.size}.by(-1)
+    	end
+    end
+
+  end
+
+
 end
 
 
