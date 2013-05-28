2be6ec4d1fcc89674d1b4e910b0fea2919be4e55
Apprentice <apprentice@devbootcamp.com>
2013-05-28 15:38:12 -0700

    removed title from comment in factory

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :post_with_comments, :parent => :post do
     after(:create) do |post|
       3.times do
-        post.comments.create(title: "dope", comment: "This is dope")
+        post.comments.create(comment: "This is dope")
       end
     end
   end
