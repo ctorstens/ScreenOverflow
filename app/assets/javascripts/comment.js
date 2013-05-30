6ccd7c0872a84cffa302ac219023ae912f410d1b
Apprentice <apprentice@devbootcamp.com>
2013-05-30 13:38:34 -0700

    working on comment formatting

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   $('#new_comment').on('ajax:success', function(xhr, data, status){
-    console.log(data)
+    console.log(data);
     var comment = $(data);
     $('.comments').prepend(comment);
     $('#comment_comment').val('');
     commentUpVoteSuccess();
     commentDownVoteSuccess();
   });
+
 });
