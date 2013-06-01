2c9cfe3be7c9d47be48334ffa6f2e03f58cdf0fd
Chris Torstenson <itunes.torstenson@gmail.com>
2013-06-01 15:08:12 -0700

    minor formatting changes

--- app/controllers/newsfeeds_controller.rb
+++ app/controllers/newsfeeds_controller.rb
@@ -3,4 +3,5 @@ class NewsfeedsController < ApplicationController
   def index
     @activities = PublicActivity::Activity.order("created_at desc").where(key: ["post.create", "comment.create"])
   end
+
 end
