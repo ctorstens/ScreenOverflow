d5e403e93717a11c323de8c6253a95b1bd1ce4ef
Chris Torstenson <itunes.torstenson@gmail.com>
2013-06-04 10:17:01 -0700

    Initial Commit with Rails boilerplate code.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScreenOverflow::Application.config.secret_token = '1785f668c3ef4c1125307555149144d26769131e3d0191188420816967605d56785cd306463dcdbd43eba6be1d35309381b0513d0c0dbb77e8df5b6f3b85261a'
